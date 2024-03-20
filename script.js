d016530a0a6634ec81022d6128734a83ce4e4920
Ouk Outdam <outdam.ouk103@gmail.com>
2024-03-20 16:00:43 +0700

    Add Buttons and Display
    Added buttons using html and css. All the buttons share the class ".square-btn". The Display is implemented using a div.

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function mul(num1, num2){
 function div(num1, num2){
     return num1 / num2;
 }
+
+function operate(){
+    if (operation === null){
+        return null;
+    }
+
+    switch (operation){
+        case "+":
+            return add(var1, var2);
+            break;
+        case "-":
+            return sub(var1, var2);
+            break;
+        case "*":
+            return mul(var1, var2);
+            break;
+        case "/":
+            return divide(var1, var2);
+            break;
+    }
+
+    return null;
+}
