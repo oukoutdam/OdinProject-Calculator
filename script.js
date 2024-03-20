14cb8b0d9373956ece498c7512bd20dc4135c989
Ouk Outdam <outdam.ouk103@gmail.com>
2024-03-20 14:06:39 +0700

    Implement Simple Operations
    Created the add, sub, mul and div functions.

---
+++ script.js
@@ -0,0 +1,14 @@
+let var1, var2, operation;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+function sub(num1, num2){
+    return num1 - num2;
+}
+function mul(num1, num2){
+    return num1 * num2;
+}
+function div(num1, num2){
+    return num1 / num2;
+}
